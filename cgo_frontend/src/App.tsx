1f3777c7a3efa87ecabff2b48c390d97952ad9c1
Nikita Nikitins <59967050+Sinet2000@users.noreply.github.com>
2023-05-18 21:33:37 +0300

    Removed submodule - moved to simple folder

---
+++ cgo_frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Typography } from 'antd';
+
+const { Title } = Typography;
+
+const App: React.FC = () => {
+      return (
+    <div className="App">
+      <Title>Hello, World!</Title>
+    </div>
+  );
+}
+
+export default App;
